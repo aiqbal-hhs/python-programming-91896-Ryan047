6af0d863fa4e0f78d4b965cf3f89cb20110ac9cd
Ryan047 <65574072+Ryan047@users.noreply.github.com>
2021-06-23 11:29:28 +1200

    Add files via upload

---
+++ Mood Tester.py
@@ -0,0 +1,28 @@
+moods = [["happy", "good"], ["sad", "bad"]]
+
+mood = input("How are you?")
+
+if mood in moods[0]:
+    print("That is good to hear.")
+if mood in moods[1]:
+    print("I do not care.")
+else:
+    print("i ain't reading all that")
+    print("i'm happy for u tho")
+    print("or sorry that happened")
+
+
+value = int(input("How old are you?"))
+if value < 0 or value > 150:
+    print("LIAR")
+if value == 0 or 1:
+    ("How are you typing this?")
+
+if value > 1  or value < 19:
+    print("You're Pretty Young.")
+
+if value > 19 or value < 30:
+    print("You're Pretty Mature")
+
+if value > 30 or value < 150:
+    print("You're Kinda old.")
