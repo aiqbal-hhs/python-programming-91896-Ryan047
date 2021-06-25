6f5889f337d70a93ba4eeafc377ca051508ab551
Ryan047 <65574072+Ryan047@users.noreply.github.com>
2021-06-25 14:45:47 +1200

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,55 @@
+# Print numbers 0 - 8
+for i in range(9):
+  print(i)
+
+
+# Print "I will practice coding every day!" 5 times
+x = ("I will practice coding everyday!" )
+for i in range(5):
+  print(x)
+
+
+# Give yourself 3 cheers
+for i in range(3):
+    print(i)
+    print("Hip hip...")
+    print("Hooray!")
+
+#Print numbers 1-10
+for i in range(1 ,11):
+  print(i)
+
+
+print() # Don't edit this one - it's just to put a gap between your loops!
+#Print numbers 20-35
+for i in range(20 ,36):
+  print(i)
+
+
+print() # Don't edit this one - it's just to put a gap between your loops!
+#Print numbers 9-18 in a sentence
+
+for i in range(9 ,19):
+  print("The next number is {}".format(i))
+
+print("------- Loop #1 --------")
+for i in range(0, 20, 2):
+  print(i)
+  
+print("------- Loop #2 --------")
+for i in range(6, 22, 3):
+  print(i)
+
+print("------- Loop #3 --------")
+for i in range(10, 0, -1):
+  print(i)
+
+
+print("------- Loop #4 --------")
+for i in range(100, -1, -10):
+  print(i)
+
+#Print 3 times table
+for i in range(1, 13):
+  print("3 x {} = {}".format(i, i * 3))
+
